5e2c28ac985c3b3962e5ecc4fdf28dee3eeac205
facubeltran <90360846+facubeltran@users.noreply.github.com>
2021-11-08 17:55:17 -0300

    a
    a

---
+++ registros.sql
@@ -0,0 +1,12 @@
+use cerveceria;
+
+insert into empleados (id, nombre, apellido, horas_trabajadas)
+values 	('8','Juan','Mendoza','4'),
+		('2','Alberto','Macri','4'),
+		('3','Mauricio','Fernandez','5'),
+		('4','Ernesto','Guevara','4'),
+		('5','Martin','Tincho','4'),
+		('6','Javier','Mitre','8'),
+		('7','Carlos','Mendoza','8');
+        
+select * from empleados;
